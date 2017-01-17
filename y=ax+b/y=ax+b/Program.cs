56f2f663719793781078ad09dc5e005ff7c34085
nalbandyan.1991 <nalbandyan.1991@bk.ru>
2017-01-17 16:34:44 +0400

    First Level

---
+++ y=ax+b/y=ax+b/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace y_ax_b
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+           
+            
+            Console.Write("A=");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("B=");
+            double b = double.Parse(Console.ReadLine());
+            double dx = 0.5;
+            double x=-5;
+            int sum = 0;
+
+            while
+                (x<=5)
+            {
+                Console.Write("X=");
+                Console.Write(x);
+                Console.Write("     Y=");
+                Console.WriteLine( (a*x * x) + b);
+               
+                x += dx;
+               
+            }
+
+
+        }
+    }
+}
